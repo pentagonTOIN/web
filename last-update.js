e7dc9201f4a91a0edeca3750d4f04a860938adf2
pentagonTOIN <toinpentagon@gmail.com>
2025-07-04 18:41:57 +0900

    Update last-update.js

--- last-update.js
+++ last-update.js
@@ -1,1 +1,16 @@
+const repoOwner="pentagonTOIN";
+const repoName="web";
 
+fetch("https://api.github.com/repos/pentagonTOIN/web/commits")
+  .then(response=>response.json())
+  .then(data=>{
+    const date=new Date(data[0].commit.committer.date);
+    const formattedDate=date.getFullYear()+"年"+
+                        (date.getMonth()+1)+"月"+
+                        date.getDate()+"日";
+    document.getElementById("last-updated").textContent="最終更新日："+formattedDate;
+  })
+  .catch(error=>{
+    console.error("取得失敗：",error);
+    document.getElementByTd("last-updated").textContent="最終更新日：取得失敗";
+  });

7a4373f77cfb31e43ce4d29f3545e8fe7ebdd2e8
pentagonTOIN <toinpentagon@gmail.com>
2025-07-23 10:07:23 +0900

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,1 +1,7 @@
-
+document.addEventListener("DOMContenLoaded",function(){
+    fetch("naver.html")
+    .then(respone => respone.text())
+    .then(data=>{
+        document.getElementById("navbar-cotainer").innerHTML=data;
+    });
+});
